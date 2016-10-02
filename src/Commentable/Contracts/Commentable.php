01bc6f2b79240797e9a4514afb53ec3be46563e8
Marc Boisvert-Dupras <bladetecltd@gmail.com>
2016-10-02 05:27:48 -0400

    Add contract

---
+++ src/Commentable/Contracts/Commentable.php
@@ -0,0 +1,11 @@
+<?php
+namespace ThyagoBrejao\Commentable\Contracts;
+use Illuminate\Database\Eloquent\Model;
+interface Commentable
+{
+    public function comments();
+    public function comment($data, Model $creator, Model $parent = null);
+    public function updateComment($id, $data, Model $parent = null);
+    public function deleteComment($id);
+    public function commentCount();
+}
